c53e44700e18bf93d552dc05469801b0834e953e
arijdb <ariane.jurado@gmail.com>
2019-04-15 19:50:41 +0200

    indent

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 "use strict";
 
-//elemtnos que usar: .field .btn .list
-//fetch a la api
-//listener al boton:
-//value del input buscarlo en el api
-// nueva url con la base mas el value (usuaria)
-// buscar la propiedad nombre real del usuario
-//ese nombre dividirlo en letras
-//cada letra escribirla en un li dentro de ol
 
 const input = document.querySelector(".field");
 const button = document.querySelector(".btn");
@@ -23,16 +15,15 @@ const getName = () => {
       const split = (data.name).split(' ');
       const name = split[0];
       const nameSplit = name.split('');
-      
-    console.log(nameSplit);
+
     list.innerHTML = '';
+
     for (const items of nameSplit) {
-    const listElement = document.createElement('li');
-    const letter = document.createTextNode(items);
-    listElement.appendChild(letter);
-    list.appendChild(listElement);
-    // listElement.classList.add(itemList);
-    listElement.setAttribute('class', 'listItem');
+      const listElement = document.createElement('li');
+      const letter = document.createTextNode(items);
+      listElement.appendChild(letter);
+      list.appendChild(listElement);
+      listElement.setAttribute('class', 'listItem');
     }
     });
 };
